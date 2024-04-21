b6c1431c916b3e165bfd033a7ad6e670bb502282
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-04-21 16:49:41 +0700

    update the test

---
+++ test/db/entity/user-entity.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024.
+ * Created by Andy Pangaribuan <https://github.com/apangaribuan>.
+ *
+ * This product is protected by copyright and distributed under
+ * licenses restricting copying, distribution and decompilation.
+ * All Rights Reserved.
+ */
+
+package entity
+
+import (
+	"time"
+
+	"github.com/andypangaribuan/gmod/fc"
+)
+
+type User struct {
+	CreatedAt  time.Time  `db:"created_at"`
+	UpdatedAt  time.Time  `db:"updated_at"`
+	DeletedAt  *time.Time `db:"deleted_at"`
+	Uid        string     `db:"uid"`
+	Name       string     `db:"name"`
+	Address    *string    `db:"address"`
+	Height     *int       `db:"height"`
+	GoldAmount *fc.FCT    `db:"gold_amount"`
+}
---
+++ test/res/table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    created_at  TIMESTAMP(3)  WITH TIME ZONE NOT NULL,
+    updated_at  TIMESTAMP(3)  WITH TIME ZONE NOT NULL,
+    deleted_at  TIMESTAMP(3)  WITH TIME ZONE,
+    uid         VARCHAR(20)   NOT NULL,
+    name        VARCHAR(500)  NOT NULL,
+    address     VARCHAR(500),
+    height      INTEGER,
+    gold_amount NUMERIC(25, 15)
+);
