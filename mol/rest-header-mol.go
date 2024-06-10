ad2f0cacd41a716f1ae1f37a0e2e8740dc893cb1
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-06-10 21:27:02 +0700

    update

--- mol/rest-header-mol.go
+++ mol/rest-header-mol.go
@@ -12,13 +12,12 @@ package mol
 import "time"
 
 type RequestHeader struct {
+	Authorization string `json:"Authorization"`
 	UID           string `json:"X-UID"`
 	Language      string `json:"X-Language"`
 	Version       string `json:"X-Version"`
-	SvcParent     string `json:"X-SvcParent"`
-	RFTime        *time.Time
-	RFTimeRaw     string `json:"X-RFTime"` // supported format: "2006-01-02T15:04:05+07:00" or "2006-01-02 15:04:05+07:00"
 	HashKey       string `json:"X-HashKey"`
-	Authorization string `json:"Authorization"`
 	UserHashId    string `json:"X-UserHashId"`
+	RFTimeRaw     string `json:"X-RFTime"` // supported format: "2006-01-02T15:04:05+07:00" or "2006-01-02 15:04:05+07:00"
+	RFTime        *time.Time
 }
