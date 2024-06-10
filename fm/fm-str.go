dbbdbeda7cb940cecef0577f6f345dab5a3aebcf
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-06-11 00:55:51 +0700

    many update

--- fm/fm-str.go
+++ fm/fm-str.go
@@ -19,3 +19,67 @@ func TrimSpace(val *string) *string {
 	v := strings.TrimSpace(*val)
 	return &v
 }
+
+func StringEqual(val *string, compare string) bool {
+	if val == nil {
+		return false
+	}
+
+	return *val == compare
+}
+
+func MultiTrimSpace(vals ...any) {
+	for _, val := range vals {
+		switch v := val.(type) {
+		case *string:
+			if v != nil {
+				*v = strings.TrimSpace(*v)
+			}
+
+		case **string:
+			if v != nil {
+				x := *v
+				if x != nil {
+					**v = strings.TrimSpace(*x)
+				}
+			}
+		}
+	}
+}
+
+func MultiToLower(vals ...any) {
+	for _, val := range vals {
+		switch v := val.(type) {
+		case *string:
+			if v != nil {
+				*v = strings.ToLower(*v)
+			}
+
+		case **string:
+			if v != nil {
+				x := *v
+				if x != nil {
+					**v = strings.ToLower(*x)
+				}
+			}
+		}
+	}
+}
+
+func FindEmptyString(vals map[string]any) string {
+	for k, val := range vals {
+		switch v := val.(type) {
+		case string:
+			if v == "" {
+				return k
+			}
+
+		case *string:
+			if v != nil && *v == "" {
+				return k
+			}
+		}
+	}
+
+	return ""
+}
