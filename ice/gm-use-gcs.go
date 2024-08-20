e92887a89a1864bcf8cecc77ace930be632ebb7b
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-08-20 18:38:43 +0700

    update

---
+++ ice/gm-use-gcs.go
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024.
+ * Created by Andy Pangaribuan <https://github.com/apangaribuan>.
+ *
+ * This product is protected by copyright and distributed under
+ * licenses restricting copying, distribution and decompilation.
+ * All Rights Reserved.
+ */
+
+package ice
+
+type UseGcs interface {
+	Init(key string, credential UtilEnvBase64, bucketName string) error
+	Get(key string) UseGcs
+}
