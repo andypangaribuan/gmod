56349821db33bcecedc4e6412effaf43a7b879d5
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-10-08 15:17:34 +0700

    move to simple grpc health

--- server/fuse-g-router-impl.go
+++ server/fuse-g-router-impl.go
@@ -10,8 +10,6 @@
 package server
 
 import (
-	"time"
-
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -25,25 +23,32 @@ func (slf *stuFuseGRouter) Server() *grpc.Server {
 	return slf.fnGetServer()
 }
 
+// reference
+// - https://github.com/grpc/grpc-go/blob/master/examples/features/health/server/main.go
+// - https://github.com/hashicorp/go-plugin/blob/main/grpc_server.go
 func (slf *stuFuseGRouter) RunHealthCheck() {
+	// server := health.NewServer()
+	// grpc_health_v1.RegisterHealthServer(slf.Server(), server)
+
+	// go func() {
+	// 	var (
+	// 		duration = time.Second
+	// 		next     = grpc_health_v1.HealthCheckResponse_SERVING
+	// 	)
+
+	// 	for {
+	// 		server.SetServingStatus("", next)
+	// 		if next == grpc_health_v1.HealthCheckResponse_SERVING {
+	// 			next = grpc_health_v1.HealthCheckResponse_NOT_SERVING
+	// 		} else {
+	// 			next = grpc_health_v1.HealthCheckResponse_SERVING
+	// 		}
+
+	// 		time.Sleep(duration)
+	// 	}
+	// }()
+
 	server := health.NewServer()
+	server.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
 	grpc_health_v1.RegisterHealthServer(slf.Server(), server)
-
-	go func() {
-		var (
-			duration = time.Second * 3
-			next     = grpc_health_v1.HealthCheckResponse_SERVING
-		)
-
-		for {
-			server.SetServingStatus("", next)
-			if next == grpc_health_v1.HealthCheckResponse_SERVING {
-				next = grpc_health_v1.HealthCheckResponse_NOT_SERVING
-			} else {
-				next = grpc_health_v1.HealthCheckResponse_SERVING
-			}
-
-			time.Sleep(duration)
-		}
-	}()
 }
